2da25667c363de3cadcc2de25e6aadccf1f50cfd
laurimus <lauridsmaentz@gmail.com>
2024-11-14 21:05:49 +0100

    Lol2

--- Python Networking/server.py
+++ Python Networking/server.py
@@ -3,9 +3,11 @@ import random
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = '0.0.0.0'
-port = int(input("Please Enter a port(if empty its <44844>)"))       
+port = input("Please Enter a port(if empty its <44844>)")       
 if port == "":
     port = 44844
+else:
+    port = int(port)
 server_socket.bind((host, port))
 server_socket.listen(2)
 
